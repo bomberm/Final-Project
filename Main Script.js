e5bc4431df9a3855a20416f256106ae60f4caccb
Marie Bomber <m.bomber2@students.clark.edu>
2016-07-30 13:48:24 -0700

    Beginnings of mysql and style setup

--- Main Script.js
+++ Main Script.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app= express();
 var handlebars = require('express-handlebars').create({defaultLayout:'default'});
 var parser = require('body-parser');
+var mysql = require(./server.js);
 
 //Express and Parser initialization
 app.engine('handlebars', handlebars.engine);
