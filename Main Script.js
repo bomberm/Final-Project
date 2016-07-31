24884bd8ecf32669895235d60e88391d75d81a13
Marie Bomber <m.bomber2@students.clark.edu>
2016-07-31 12:16:09 -0700

    Add error handlers

--- Main Script.js
+++ Main Script.js
@@ -15,6 +15,15 @@ app.set('port', 3000);
 app.get('/', function(req, res){
   res.render('exercise');
   });
+  
+  app.use(function(req,res){
+  res.render('oops404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.render('embarrassing500');
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
